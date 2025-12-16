72ef11d2d7cd02f24bd002a86b572f938e832d02
Gustavo Alarcón <151586870+GustavoAlarcon95@users.noreply.github.com>
2025-12-16 18:21:27 -0300

    inicial

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <hr />
+        <p>Desarrolado por: Gustavo Alarcon</p>
+    </footer>
+  )
+}
+
+export default Footer
