da8bbfb588c9fc733c194e6e352003eb28155f8a
Gustavo Alarcón <151586870+GustavoAlarcon95@users.noreply.github.com>
2025-12-16 19:10:25 -0300

    inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,26 +4,15 @@ function Inicio() {
   return (
     <>
     <div style={{textAlign: 'center'}}>
-    <h1>| Juegos de Mesa |  </h1>
+    <h1>| Aires Acondicionados |  </h1>
     <hr />
     <main>
-      <p>El <strong>Senet</strong> fue un juego muy popular en el antiguo Egipto.</p> <p>Además del entretenimiento, poseía un profundo sentido ritual vinculado a la vida eterna. Los egipcios creían que desarrollar habilidades estratégicas para controlar el azar durante el juego era una forma de prepararse para justificar sus acciones en la vida y ante los dioses.</p>
+      <p>El <strong>aire acondicionado.</strong>n la antiguedad, los egipcios ya utilizaban sistemas y métodos para reducir el calor. Se utilizaba principalmente en el palacio del faraón, cuyas paredes estaban formadas por enormes bloques de piedra, con un peso superior a mil toneladas</p> <p>En 1842, Lord Kelvin inventó el principio del aire acondicionado. En 1902, el estadounidense Willis Haviland Carrier sentó las bases de la refrigeración moderna al encontrarse con los problemas de la excesiva humidificación del aire enfriado, las del aire acondicionado, desarrollando el concepto de climatización de verano.</p>
     </main>
 
-    <img src="https://www.glueckshaendler.de/wp-content/uploads/2020/09/Senet-Brettspiel-aus-Holz-mit-Spielsteinen.jpg"></img>
+    <img src="https://www.elaireacondicionado.com/sites/elaireacondicionado.com/files/images/articulos/maquina-refrigeracion-centrifuga.jpg"></img>
     <br />
-        <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi1jKo9c_Vh2Rsg0jUj4idDOhBge3smcm8qrjmJklxoNe9vPpFSKJWhfi00lMaGrpn3oyAABwhhVwrxxnDk0Vsvno-HiGhOuUXN0mC_42I8CLswGKJzUHgl-UqtkDdEf5SsbmsAHg9CxZ0/s320/492ca122590140b5b1366be3336cd2d3.jpg" width="20%"></img>
     <br />
-    <a href="https://www.man.es/man/en/educacion/recursos/juegos.html" target="_blank">[+] info</a>
-    <br />
-    <hr />
-    <a href="https://play.google.com/store/apps/details?id=air.EgyptianSenetFree&hl=es">Android</a>
-    <br />
-    <a href="https://apps.apple.com/us/app/egyptian-senet/id598710611">IOS</a>
-    <br />
-
-    <br />
-    <a href="https://www.danielschavelzon.com.ar/?p=2927" target='_blank'>Juegos en Tiempos de la Colonia</a>
     </div>
     </>
   )
